8e7c7d0c7268c6edfb8754baf28b7e460c150a14
Hsiangting <stpeng777@gmail.com>
2026-01-20 21:19:00 -0800

    build of Spring Boot backend skeleton

---
+++ src/main/java/com/mindoasis/backend/controller/HealthController.java
@@ -0,0 +1,21 @@
+package com.mindoasis.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
+
+public class HealthController{
+
+    @GetMapping("/health")
+    public Map<String, String> healthCheck() {
+        return Map.of("status", "Mind Oasis Backend is Running");
+
+    }
+}
