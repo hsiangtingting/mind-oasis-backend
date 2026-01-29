8b71eb0323e61ed82a6b63922a674a9f96bf87b4
Hsiangting <stpeng777@gmail.com>
2026-01-28 16:21:35 -0800

    implement calling Met API

---
+++ src/main/java/com/mindoasis/backend/controller/ArtController.java
@@ -0,0 +1,22 @@
+package com.mindoasis.backend.controller;
+
+import com.mindoasis.backend.dto.MetArtworkDTO;
+import com.mindoasis.backend.service.MetApiService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/art")
+@CrossOrigin(origins = "*")
+public class ArtController {
+
+    private final MetApiService metApiService;
+
+    public ArtController(MetApiService metApiService) {
+        this.metApiService = metApiService;
+    }
+
+    @GetMapping("/recommend")
+    public MetArtworkDTO getRecommendation(@RequestParam String theme) {
+        return metApiService.getArtworkByTheme(theme);
+    }
+}
